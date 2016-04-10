b95473fa2c3649b5de81a5beddbfe89368d129b0
Hector Rivas Gandara <keymon@gmail.com>
2016-04-10 13:12:49 +0100

    Test with YAML concourse resource definitions
    In order to ease testing, parse and read from concourse YAML
    resource definitions to build the commands requests.

--- check/check_command_test.go
+++ check/check_command_test.go
@@ -1,12 +1,40 @@
 package check_test
 
 import (
+	"fmt"
+	"github.com/ghodss/yaml"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	. "github.com/redfactorlabs/concourse-smuggler-resource"
 	. "github.com/redfactorlabs/concourse-smuggler-resource/check"
 )
+
+func checkRequestFromYaml(yaml_manifest string, resource_name string) (*CheckRequest, error) {
+	var resourceDefinition ResourceDefinition
+	err := yaml.Unmarshal([]byte(yaml_manifest), &resourceDefinition)
+	if err != nil {
+		return nil, err
+	}
+
+	var resource *Resource
+	for _, r := range resourceDefinition.Resources {
+		if r.Name == resource_name {
+			resource = &r
+		}
+	}
+	if resource == nil {
+		return nil, fmt.Errorf("Cannot find a resource called '%s' in the yaml definition.", resource_name)
+	}
+
+	var checkRequest = CheckRequest{
+		Source: resource.Source,
+	}
+
+	return &checkRequest, nil
+
+}
 
 var _ = Describe("Check Command", func() {
 	It("executes a basic echo command", func() {
@@ -26,7 +54,24 @@ var _ = Describe("Check Command", func() {
 		checkCommand.Run(requestBasicEcho)
 		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("basic echo test"))
 	})
+	It("executes a basic echo command from yaml manifest and returns the output", func() {
+		requestBasicEcho, err := checkRequestFromYaml(resourceDefinitionBasicEcho, "simple_echo")
+		Ω(err).ShouldNot(HaveOccurred())
+		checkCommand := NewCheckCommand()
+		checkCommand.Run(*requestBasicEcho)
+		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("basic echo test"))
+	})
 })
+
+type ResourceDefinition struct {
+	Resources []Resource `json: resources`
+}
+
+type Resource struct {
+	Name   string `json:name`
+	Type   string `json:type`
+	Source Source `json:source`
+}
 
 var requestBasicEcho = CheckRequest{
 	Source: Source{
@@ -51,4 +96,19 @@ var requestBasicEchoJson = `
   },
   "version": {}
 }
+`
+
+var resourceDefinitionBasicEcho = `
+resources:
+- name: simple_echo
+  type: smuggler
+  source:
+    smuggler_config:
+      check:
+        path: sh
+        args:
+        - -e
+        - -c
+        - |
+          echo basic echo test
 `
