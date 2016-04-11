cde09e526447f74a37897aff5490b187042d31d9
Hector Rivas Gandara <keymon@gmail.com>
2016-04-11 23:38:37 +0100

    Simplify Out tests

--- out_command_test.go
+++ out_command_test.go
@@ -9,90 +9,10 @@ import (
 )
 
 var _ = Describe("Out Command", func() {
-	It("it gets the sources dir", func() {
+	Context("when given a config with a complex script from yaml", func() {
 		manifest := `
 resources:
-- name: output_dir
-  type: smuggler
-  source:
-    commands:
-    - name: out
-      path: bash
-      args:
-      - -e
-      - -c
-      - |
-        echo "sourcesDir=$SMUGGLER_SOURCES_DIR"
-        echo "1.2.3" > $SMUGGLER_OUTPUT_DIR/version
-`
-		source, err := ResourceSourceFromYamlManifest(manifest, "output_dir")
-		Ω(err).ShouldNot(HaveOccurred())
-		request := OutRequest{
-			Source: *source,
-		}
-		command := NewSmugglerCommand()
-		_, err = command.RunOut("/tmp/sources/dir", request)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("sourcesDir=/tmp/sources/dir"))
-	})
-
-	It("it fails if it cannot find the version ID", func() {
-		manifest := `
-resources:
-- name: out_command
-  type: smuggler
-  source:
-    commands:
-    - name: out
-      path: bash
-      args:
-      - -e
-      - -c
-      - |
-        true
-`
-		source, err := ResourceSourceFromYamlManifest(manifest, "out_command")
-		Ω(err).ShouldNot(HaveOccurred())
-		request := OutRequest{
-			Source: *source,
-		}
-		command := NewSmugglerCommand()
-		_, err = command.RunOut("", request)
-		Ω(err).Should(HaveOccurred())
-	})
-	It("it exports the version ID", func() {
-		manifest := `
-resources:
-- name: out_command
-  type: smuggler
-  source:
-    commands:
-    - name: out
-      path: bash
-      args:
-      - -e
-      - -c
-      - |
-        echo "1.2.3" > $SMUGGLER_OUTPUT_DIR/version
-        echo -e "\t 1.2.3   " >> $SMUGGLER_OUTPUT_DIR/version
-        echo -e "\t 1.2.4   " >> $SMUGGLER_OUTPUT_DIR/version
-`
-		source, err := ResourceSourceFromYamlManifest(manifest, "out_command")
-		Ω(err).ShouldNot(HaveOccurred())
-		request := OutRequest{
-			Source: *source,
-		}
-		command := NewSmugglerCommand()
-		outResponse, err := command.RunOut("", request)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		Ω(outResponse.Version).Should(BeEquivalentTo(Version{VersionID: "1.2.3"}))
-	})
-	It("passes the resource params as environment variables", func() {
-		manifest := `
-resources:
-- name: pass_params
+- name: complex_command
   type: smuggler
   source:
     extra_params:
@@ -101,33 +21,69 @@ resources:
       param3: 123
     commands:
     - name: out
-      path: sh
+      path: bash
       args:
       - -e
       - -c
       - |
+        echo Command Start
+        echo "sourcesDir=$SMUGGLER_SOURCES_DIR"
         echo "param1=${SMUGGLER_param1}"
         echo "param2=${SMUGGLER_param2}"
         echo "param3=${SMUGGLER_param3}"
         echo "param4=${SMUGGLER_param4}"
         echo "param5=${SMUGGLER_param5}"
         echo "1.2.3" > ${SMUGGLER_OUTPUT_DIR}/version
+        echo "value1= something quite long  " > ${SMUGGLER_OUTPUT_DIR}/metadata
+        echo -e "\n   " >> ${SMUGGLER_OUTPUT_DIR}/metadata
+        echo -e "\t value_2=2  \n" >> ${SMUGGLER_OUTPUT_DIR}/metadata
+        echo Command End
 `
-		source, err := ResourceSourceFromYamlManifest(manifest, "pass_params")
-		Ω(err).ShouldNot(HaveOccurred())
-		request := OutRequest{
-			Source: *source,
-			Params: map[string]string{
-				"param4": "val4",
-				"param5": "something with spaces",
-			},
-		}
-		checkCommand := NewSmugglerCommand()
-		checkCommand.RunOut("", request)
-		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
-		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
-		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
-		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("param4=val4"))
-		Ω(checkCommand.LastCommandCombinedOuput()).Should(ContainSubstring("param5=something with spaces"))
+		var request OutRequest
+		var command *SmugglerCommand
+		var response OutResponse
+
+		BeforeEach(func() {
+			source, err := ResourceSourceFromYamlManifest(manifest, "complex_command")
+			Ω(err).ShouldNot(HaveOccurred())
+			request = OutRequest{
+				Source: *source,
+				Params: map[string]string{
+					"param4": "val4",
+					"param5": "something with spaces",
+				},
+			}
+			command = NewSmugglerCommand()
+			response, err = command.RunOut("/tmp/sources/dir", request)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+		It("executes several lines of the script", func() {
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command Start"))
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command End"))
+		})
+
+		It("it sets the resource extra_params and 'put' params as environment variables", func() {
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param4=val4"))
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param5=something with spaces"))
+		})
+
+		It("it gets the sources dir", func() {
+			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("sourcesDir=/tmp/sources/dir"))
+		})
+
+		It("it return the version ID", func() {
+			Ω(response.Version).Should(BeEquivalentTo(Version{VersionID: "1.2.3"}))
+		})
+
+		It("it returns metadata as list of strings", func() {
+			vs := []MetadataPair{
+				MetadataPair{Name: "value1", Value: "something quite long"},
+				MetadataPair{Name: "value_2", Value: "2"},
+			}
+			Ω(response.Metadata).Should(BeEquivalentTo(vs))
+		})
 	})
 })
