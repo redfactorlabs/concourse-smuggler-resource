865ec5b9ea2e419587b9f34a5cfe97717173be47
Hector Rivas Gandara <keymon@gmail.com>
2016-04-11 01:07:14 +0100

    Implement check command

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -3,9 +3,33 @@ package check
 import (
 	"encoding/json"
 	"os"
+
+	"github.com/redfactorlabs/concourse-smuggler-resource"
 )
 
 func main() {
-	// no-op check
-	json.NewEncoder(os.Stdout).Encode([]interface{}{})
+	var request smuggler.CheckRequest
+	inputRequest(&request)
+
+	command := smuggler.NewSmugglerCommand()
+
+	response, err := command.RunCheck(request)
+	if err != nil {
+		smuggler.Fatal("running command", err)
+	}
+
+	outputResponse(response)
+
+}
+
+func inputRequest(request *smuggler.CheckRequest) {
+	if err := json.NewDecoder(os.Stdin).Decode(request); err != nil {
+		smuggler.Fatal("reading request from stdin", err)
+	}
+}
+
+func outputResponse(response smuggler.CheckResponse) {
+	if err := json.NewEncoder(os.Stdout).Encode(response); err != nil {
+		smuggler.Fatal("writing response to stdout", err)
+	}
 }
