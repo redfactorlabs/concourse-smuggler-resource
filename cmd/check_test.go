61d3c692d09e2d8f43951cf0e3297a0b3c82342d
Hector Rivas Gandara <keymon@gmail.com>
2016-04-13 23:51:06 +0100

    Refactor check cmd to allow reuse them for in/out

--- cmd/check_test.go
+++ cmd/check_test.go
@@ -21,28 +21,42 @@ import (
 var manifest = Fixture("pipeline.yml")
 var err error
 
-var _ = Describe("check", func() {
+var _ = Describe("smuggler commands", func() {
 	var (
-		command *exec.Cmd
-		stdin   *bytes.Buffer
 		session *gexec.Session
 		logFile *os.File
 
+		commandPath        string
 		expectedExitStatus int
+		request            ResourceRequest
 	)
 
 	BeforeEach(func() {
-		var err error
-		stdin = &bytes.Buffer{}
 		expectedExitStatus = 0
+	})
 
-		command = exec.Command(checkPath)
+	JustBeforeEach(func() {
+		var err error
+		var command *exec.Cmd
+
+		stdin := &bytes.Buffer{}
+		err = json.NewEncoder(stdin).Encode(request)
+		Ω(err).ShouldNot(HaveOccurred())
+
+		command = exec.Command(commandPath)
 		command.Stdin = stdin
 
 		// Point log file to a temporary location
 		logFile, err = ioutil.TempFile("", "smuggler.log")
 		Ω(err).ShouldNot(HaveOccurred())
 		command.Env = append(os.Environ(), fmt.Sprintf("SMUGGLER_LOG=%s", logFile.Name()))
+
+		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Ω(err).ShouldNot(HaveOccurred())
+
+		<-session.Exited
+		Expect(session.ExitCode()).To(Equal(expectedExitStatus))
+
 	})
 
 	AfterEach(func() {
@@ -52,77 +66,66 @@ var _ = Describe("check", func() {
 		os.Remove(logFile.Name())
 	})
 
-	JustBeforeEach(func() {
-		var err error
-		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Ω(err).ShouldNot(HaveOccurred())
+	Context("when given a complex definition", func() {
+		Context("for the 'check' command", func() {
+			BeforeEach(func() {
+				commandPath = checkPath
 
-		<-session.Exited
-		Expect(session.ExitCode()).To(Equal(expectedExitStatus))
-	})
+				request, err = GetResourceRequestFromYamlManifest(CheckType, manifest, "complex_command", "a_job")
+				Ω(err).ShouldNot(HaveOccurred())
 
-	Context("when given a complex command", func() {
-		var request ResourceRequest
+			})
+			It("outputs a valid json with a version", func() {
+				var response []Version
+				err := json.Unmarshal(session.Out.Contents(), &response)
+				Ω(err).ShouldNot(HaveOccurred())
+				vs := []Version{Version{VersionID: "1.2.3"}, Version{VersionID: "1.2.4"}}
+				Ω(response).Should(BeEquivalentTo(vs))
+			})
 
-		BeforeEach(func() {
-			request, err = GetResourceRequestFromYamlManifest(CheckType, manifest, "complex_command", "a_job")
-			Ω(err).ShouldNot(HaveOccurred())
+			It("outputs the commands output", func() {
+				stderr := session.Err.Contents()
 
-			err = json.NewEncoder(stdin).Encode(request)
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		It("outputs a valid json with a version", func() {
-			var response []Version
-			err := json.Unmarshal(session.Out.Contents(), &response)
-			Ω(err).ShouldNot(HaveOccurred())
-			vs := []Version{Version{VersionID: "1.2.3"}, Version{VersionID: "1.2.4"}}
-			Ω(response).Should(BeEquivalentTo(vs))
-		})
-
-		It("outputs the commands output", func() {
-			stderr := session.Err.Contents()
-
-			Ω(stderr).Should(ContainSubstring("Command Start"))
-			Ω(stderr).Should(ContainSubstring("Command End"))
-			Ω(stderr).Should(ContainSubstring("param1=test"))
-			Ω(stderr).Should(ContainSubstring("param2=true"))
-			Ω(stderr).Should(ContainSubstring("param3=123"))
+				Ω(stderr).Should(ContainSubstring("Command Start"))
+				Ω(stderr).Should(ContainSubstring("Command End"))
+				Ω(stderr).Should(ContainSubstring("param1=test"))
+				Ω(stderr).Should(ContainSubstring("param2=true"))
+				Ω(stderr).Should(ContainSubstring("param3=123"))
+			})
 		})
 	})
 
 	Context("when given a dummy command", func() {
-		var request ResourceRequest
+		Context("for the 'check' command", func() {
+			BeforeEach(func() {
+				commandPath = checkPath
 
-		BeforeEach(func() {
-			request, err = GetResourceRequestFromYamlManifest(CheckType, manifest, "dummy_command", "a_job")
+				request, err = GetResourceRequestFromYamlManifest(CheckType, manifest, "dummy_command", "a_job")
+				Ω(err).ShouldNot(HaveOccurred())
+			})
 
-			err = json.NewEncoder(stdin).Encode(request)
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		It("returns empty version list", func() {
-			var response []Version
-			err := json.Unmarshal(session.Out.Contents(), &response)
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(response).Should(BeEmpty())
+			It("returns empty version list", func() {
+				var response []Version
+				err := json.Unmarshal(session.Out.Contents(), &response)
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(response).Should(BeEmpty())
+			})
 		})
 	})
 
 	Context("when given a command which fails", func() {
-		var request ResourceRequest
+		Context("for the 'check' command", func() {
+			BeforeEach(func() {
+				expectedExitStatus = 2
+				commandPath = checkPath
 
-		BeforeEach(func() {
-			request, err = GetResourceRequestFromYamlManifest(CheckType, manifest, "fail_command", "a_job")
+				request, err = GetResourceRequestFromYamlManifest(CheckType, manifest, "fail_command", "a_job")
+				Ω(err).ShouldNot(HaveOccurred())
+			})
 
-			expectedExitStatus = 2
-
-			err = json.NewEncoder(stdin).Encode(request)
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		It("returns an error", func() {
-			Ω(session.Err).Should(gbytes.Say("error running command"))
+			It("returns an error", func() {
+				Ω(session.Err).Should(gbytes.Say("error running command"))
+			})
 		})
 	})
 })
