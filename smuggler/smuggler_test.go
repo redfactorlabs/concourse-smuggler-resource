ca3d8a56fc5174913d713c8a2ac214c1bbb3531e
Hector Rivas Gandara <keymon@gmail.com>
2016-04-13 02:11:17 +0100

    Refactor to group all Actions in one function
    Reduce the code to have only one functions for all actions,
    and reduced the test suite.

--- smuggler/smuggler_test.go
+++ smuggler/smuggler_test.go
@@ -1,9 +1,10 @@
 package smuggler_test
 
 import (
+	"log"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"log"
 
 	. "github.com/redfactorlabs/concourse-smuggler-resource/helpers/test"
 	. "github.com/redfactorlabs/concourse-smuggler-resource/smuggler"
@@ -12,9 +13,17 @@ import (
 var manifest = Fixture("pipeline.yml")
 var logger = log.New(GinkgoWriter, "smuggler: ", log.Lmicroseconds)
 
-var _ = Describe("Check Command", func() {
+var request ResourceRequest
+var response ResourceResponse
+var command *SmugglerCommand
+var fixtureResourceName string
+var requestType RequestType
+var requestVersion Version
+var err error
+
+var _ = Describe("Check Command basic tests", func() {
 	Context("when given a basic config from a structure", func() {
-		request := CheckRequest{
+		request := ResourceRequest{
 			Source: Source{
 				Commands: []CommandDefinition{
 					CommandDefinition{
@@ -24,26 +33,25 @@ var _ = Describe("Check Command", func() {
 					},
 				},
 			},
+			Type: CheckType,
 		}
 
 		It("it executes the command successfully and captures the output", func() {
 			command := NewSmugglerCommand(logger)
-			command.RunCheck(request)
+			command.RunAction("", request)
 			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("basic echo test"))
 			Ω(command.LastCommandSuccess()).Should(BeTrue())
 		})
 	})
 
 	Context("when given a basic config from a json", func() {
-		var command *SmugglerCommand
-		var response CheckResponse
 		requestJson := `{
 			"source": {
 				"commands": [
 					{
-			"name": "check",
-			"path": "sh",
-			"args": [ "-e", "-c", "echo basic echo test" ]
+						"name": "check",
+						"path": "sh",
+						"args": [ "-e", "-c", "echo basic echo test" ]
 					}
 				]
 			},
@@ -51,10 +59,10 @@ var _ = Describe("Check Command", func() {
 		}`
 
 		BeforeEach(func() {
-			request, err := NewCheckRequestFromJson(requestJson)
+			request, err = NewResourceRequestFromJson(requestJson, CheckType)
 			Ω(err).ShouldNot(HaveOccurred())
 			command = NewSmugglerCommand(logger)
-			response, err = command.RunCheck(request)
+			response, err = command.RunAction("", request)
 			Ω(err).ShouldNot(HaveOccurred())
 		})
 
@@ -63,244 +71,149 @@ var _ = Describe("Check Command", func() {
 			Ω(command.LastCommandSuccess()).Should(BeTrue())
 		})
 	})
+})
 
-	Context("when given a config with empty config from yaml", func() {
-		var request CheckRequest
-		var command *SmugglerCommand
-		var response CheckResponse
+var _ = Describe("SmugglerCommand Actions", func() {
+	JustBeforeEach(func() {
+		request, err = GetResourceRequestFromYamlManifest(requestType, manifest, fixtureResourceName, "a_job")
+		Ω(err).ShouldNot(HaveOccurred())
+		command = NewSmugglerCommand(logger)
+		response, err = command.RunAction("/some/path", request)
+	})
 
+	Context("when calling action 'check'", func() {
 		BeforeEach(func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "dummy_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = CheckRequest{
-				Source: *source,
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunCheck(request)
-			Ω(err).ShouldNot(HaveOccurred())
+			requestType = CheckType
 		})
-		It("does not execute and returns an empty response", func() {
-			Ω(command.LastCommand()).Should(BeNil())
-			Ω(command.LastCommandCombinedOuput()).Should(BeEmpty())
-			Ω(command.LastCommandSuccess()).Should(BeTrue())
-			Ω(response).Should(BeEmpty())
+
+		Context("when running CommonSmugglerTests()", CommonSmugglerTests())
+
+		Context("when given a config with a complex script from yaml", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "complex_command"
+			})
+			It("it returns versions as list of strings", func() {
+				vs := []Version{Version{VersionID: "1.2.3"}, Version{VersionID: "1.2.4"}}
+				Ω(response.Versions).Should(BeEquivalentTo(vs))
+			})
 		})
 	})
-	Context("when given a config with a complex script from yaml", func() {
-		var request CheckRequest
-		var command *SmugglerCommand
-		var response CheckResponse
 
+	Context("When calling action 'in'", func() {
 		BeforeEach(func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "complex_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = CheckRequest{
-				Source: *source,
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunCheck(request)
-			Ω(err).ShouldNot(HaveOccurred())
+			requestType = InType
 		})
-		It("executes several lines of the script", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command Start"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command End"))
-			Ω(command.LastCommandSuccess()).Should(BeTrue())
-		})
-		It("it sets the $MUGGLER_ACTION and $SMUGGLE_COMMAND variables", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("action=check"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("command=check"))
-		})
-		It("it can sets the resource extra_params as environment variables", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
-		})
-		It("it returns versions as list of strings", func() {
-			vs := []Version{Version{VersionID: "1.2.3"}, Version{VersionID: "1.2.4"}}
-			Ω(response).Should(BeEquivalentTo(vs))
+		Context("when running CommonSmugglerTests()", CommonSmugglerTests())
+
+		Context("when running InOutCommonSmugglerTests()", InOutCommonSmugglerTests())
+
+		Context("when given a config with a complex script from yaml", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "complex_command"
+			})
+
+			It("it gets the destination dir", func() {
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("destinationDir=/some/path"))
+			})
 		})
 	})
-	Context("when given a command which fails", func() {
-		var request CheckRequest
-		var command *SmugglerCommand
-		var response CheckResponse
+	Context("When calling action 'out'", func() {
+		BeforeEach(func() {
+			requestType = OutType
+		})
+		Context("when running CommonSmugglerTests()", CommonSmugglerTests())
 
-		It("captures the exit code", func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "fail_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = CheckRequest{
-				Source: *source,
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunCheck(request)
-			Ω(err).Should(HaveOccurred())
+		Context("when running InOutCommonSmugglerTests()", InOutCommonSmugglerTests())
 
-			Ω(command.LastCommandSuccess()).Should(BeFalse())
-			Ω(command.LastCommandExitStatus()).Should(Equal(2))
+		Context("when given a config with a complex script from yaml", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "complex_command"
+			})
+			It("it gets the sources dir", func() {
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("sourcesDir=/some/path"))
+			})
 		})
 	})
 })
 
-var _ = Describe("In Command", func() {
-	Context("when given a config with a complex script from yaml", func() {
-		var request InRequest
-		var command *SmugglerCommand
-		var response InResponse
+func CommonSmugglerTests() func() {
+	return func() {
+		Context("when given a config with empty config from yaml", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "dummy_command"
+			})
+			It("executes without errors", func() {
+				Ω(err).ShouldNot(HaveOccurred())
+			})
 
-		BeforeEach(func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "complex_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = InRequest{
-				Source:  *source,
-				Version: Version{VersionID: "1.2.3"},
-				Params: map[string]string{
-					"param4": "val4",
-					"param5": "something with spaces",
-				},
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunIn("/tmp/destination/dir", request)
-			Ω(err).ShouldNot(HaveOccurred())
+			It("does not execute and returns an empty response", func() {
+				Ω(command.LastCommand()).Should(BeNil())
+				Ω(command.LastCommandCombinedOuput()).Should(BeEmpty())
+				Ω(command.LastCommandSuccess()).Should(BeTrue())
+				Ω(response.IsEmpty()).Should(BeTrue())
+			})
 		})
-		It("executes several lines of the script", func() {
-			Ω(command.LastCommandSuccess()).Should(BeTrue())
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command Start"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command End"))
-		})
-		It("it sets the $MUGGLER_ACTION and $SMUGGLE_COMMAND variables", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("action=in"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("command=in"))
-		})
-		It("it sets the resource extra_params and 'get' params as environment variables", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param4=val4"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param5=something with spaces"))
-		})
+		Context("when given a config with a complex script from yaml", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "complex_command"
+			})
 
-		It("it gets the destination dir", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("destinationDir=/tmp/destination/dir"))
+			It("executes without errors", func() {
+				Ω(err).ShouldNot(HaveOccurred())
+			})
+			It("executes several lines of the script", func() {
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command Start"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command End"))
+				Ω(command.LastCommandSuccess()).Should(BeTrue())
+			})
+			It("it sets the $MUGGLER_ACTION and $SMUGGLE_COMMAND variables", func() {
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("action=" + string(requestType)))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("command=" + string(requestType)))
+			})
+			It("it can sets the resource extra_params as environment variables", func() {
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
+			})
 		})
+		Context("when given a command which fails", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "fail_command"
+			})
+			It("returns error", func() {
+				Ω(err).Should(HaveOccurred())
+			})
+			It("captures the exit code", func() {
+				Ω(command.LastCommandSuccess()).Should(BeFalse())
+				Ω(command.LastCommandExitStatus()).Should(Equal(2))
+			})
+		})
+	}
+}
 
-		It("it gets the version ID", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("version=1.2.3"))
+func InOutCommonSmugglerTests() func() {
+	return func() {
+		Context("when given a config with a complex script from yaml", func() {
+			BeforeEach(func() {
+				fixtureResourceName = "complex_command"
+			})
+			It("it sets the resource extra_params and 'get' params as environment variables", func() {
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param4=val4"))
+				Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param5=something with spaces"))
+			})
+			It("it returns metadata as list of strings", func() {
+				vs := []MetadataPair{
+					MetadataPair{Name: "value1", Value: "something quite long"},
+					MetadataPair{Name: "value_2", Value: "2"},
+				}
+				Ω(response.Metadata).Should(BeEquivalentTo(vs))
+			})
+			It("it returns the version ID", func() {
+				Ω(response.Version).Should(BeEquivalentTo(Version{VersionID: "1.2.3"}))
+			})
 		})
-
-		It("it returns metadata as list of strings", func() {
-			vs := []MetadataPair{
-				MetadataPair{Name: "value1", Value: "something quite long"},
-				MetadataPair{Name: "value_2", Value: "2"},
-			}
-			Ω(response.Metadata).Should(BeEquivalentTo(vs))
-		})
-	})
-
-	Context("when given a dummy config with empty commands from yaml", func() {
-		var request InRequest
-		var command *SmugglerCommand
-		var response InResponse
-
-		BeforeEach(func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "dummy_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = InRequest{
-				Source:  *source,
-				Version: Version{VersionID: "1.2.3"},
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunIn("/tmp/destination/dir", request)
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-		It("does not execute", func() {
-			Ω(command.LastCommand()).Should(BeNil())
-			Ω(command.LastCommandCombinedOuput()).Should(BeEmpty())
-			Ω(command.LastCommandSuccess()).Should(BeTrue())
-		})
-		It("does not execute and returns the same version", func() {
-			Ω(response.Version).Should(Equal(request.Version))
-		})
-		It("does not execute and returns empty metadata", func() {
-			Ω(response.Metadata).Should(BeEmpty())
-		})
-	})
-})
-
-var _ = Describe("Out Command", func() {
-	Context("when given a config with a complex script from yaml", func() {
-		var request OutRequest
-		var command *SmugglerCommand
-		var response OutResponse
-
-		BeforeEach(func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "complex_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = OutRequest{
-				Source: *source,
-				Params: map[string]string{
-					"param4": "val4",
-					"param5": "something with spaces",
-				},
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunOut("/tmp/sources/dir", request)
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-		It("executes several lines of the script", func() {
-			Ω(command.LastCommandSuccess()).Should(BeTrue())
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command Start"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("Command End"))
-		})
-
-		It("it sets the $MUGGLER_ACTION and $SMUGGLE_COMMAND variables", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("action=out"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("command=out"))
-		})
-		It("it sets the resource extra_params and 'put' params as environment variables", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param1=test"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param2=true"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param3=123"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param4=val4"))
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("param5=something with spaces"))
-		})
-
-		It("it gets the sources dir", func() {
-			Ω(command.LastCommandCombinedOuput()).Should(ContainSubstring("sourcesDir=/tmp/sources/dir"))
-		})
-
-		It("it return the version ID", func() {
-			Ω(response.Version).Should(BeEquivalentTo(Version{VersionID: "1.2.3"}))
-		})
-
-		It("it returns metadata as list of strings", func() {
-			vs := []MetadataPair{
-				MetadataPair{Name: "value1", Value: "something quite long"},
-				MetadataPair{Name: "value_2", Value: "2"},
-			}
-			Ω(response.Metadata).Should(BeEquivalentTo(vs))
-		})
-	})
-
-	Context("when given a dummy config with empty commands from yaml", func() {
-		var request OutRequest
-		var command *SmugglerCommand
-		var response OutResponse
-
-		BeforeEach(func() {
-			source, err := ResourceSourceFromYamlManifest(manifest, "dummy_command")
-			Ω(err).ShouldNot(HaveOccurred())
-			request = OutRequest{
-				Source: *source,
-			}
-			command = NewSmugglerCommand(logger)
-			response, err = command.RunOut("/tmp/sources/dir", request)
-			Ω(err).Should(HaveOccurred())
-		})
-		It("does not execute the command", func() {
-			Ω(command.LastCommand()).Should(BeNil())
-		})
-		It("does not execute and returns empty metadata", func() {
-			Ω(response.Metadata).Should(BeEmpty())
-		})
-	})
-})
+	}
+}
