e08370d3e79f88991d86b5d7d1f9b34a0108b79a
Hector Rivas Gandara <keymon@gmail.com>
2016-04-14 19:45:57 +0100

    Implemented one unique binary command

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,23 +1,58 @@
-package commands
+package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/redfactorlabs/concourse-smuggler-resource/helpers/utils"
 	"github.com/redfactorlabs/concourse-smuggler-resource/smuggler"
 )
 
-func SmugglerMain(dataDir string, requestType smuggler.RequestType) {
+func main() {
+	var dataDir string
+	var requestType smuggler.RequestType
+
+	// Determine which command is being called by the name
+	commandName := filepath.Base(os.Args[0])
+	switch {
+	case strings.Contains(commandName, "check"):
+		dataDir = ""
+		requestType = smuggler.CheckType
+	case strings.Contains(commandName, "in"):
+		if len(os.Args) < 2 {
+			utils.Sayf("usage: %s <dest directory>\n", os.Args[0])
+			os.Exit(1)
+		}
+		dataDir = os.Args[1]
+		requestType = smuggler.InType
+	case strings.Contains(commandName, "out"):
+		if len(os.Args) < 2 {
+			utils.Sayf("usage: %s <sources directory>\n", os.Args[0])
+			os.Exit(1)
+		}
+		dataDir = os.Args[1]
+		requestType = smuggler.OutType
+	default:
+		utils.Fatal("identifying resource type",
+			fmt.Errorf("command name '%s' does not contain check/in/out", commandName),
+			1)
+	}
+
+	// Open Log file
 	smugglerLogFileName := utils.GetEnvOrDefault("SMUGGLER_LOG", "/tmp/smuggler.log")
 	tempFileLogger, err := utils.NewTempFileLogger(smugglerLogFileName)
 	if err != nil {
 		utils.Fatal("opening log '/tmp/smuggler.log'", err, 1)
 	}
 
+	// Read request
 	request := smuggler.ResourceRequest{Type: requestType}
 	inputRequest(&request)
 
+	// Execute command
 	command := smuggler.NewSmugglerCommand(tempFileLogger.Logger)
 
 	response, err := command.RunAction(dataDir, request)
@@ -26,6 +61,7 @@ func SmugglerMain(dataDir string, requestType smuggler.RequestType) {
 	}
 	os.Stderr.Write([]byte(command.LastCommandCombinedOuput()))
 
+	// Send back response
 	if requestType == smuggler.CheckType {
 		outputResponseCheck(response.Versions)
 	} else {
--- cmd/commands_test.go
+++ cmd/commands_test.go
@@ -1,4 +1,4 @@
-package commands_test
+package main_test
 
 import (
 	"bytes"
