6d512b73d4fd9a05254bd73dab43bb237cb84dc2
Hector Rivas Gandara <keymon@gmail.com>
2016-04-16 16:23:49 +0100

    Use interface{} for the params
    This way we can pass to the commands any kind of complex structure.

--- smuggler/smuggler_test.go
+++ smuggler/smuggler_test.go
@@ -156,7 +156,7 @@ var _ = Describe("SmugglerCommand Actions", func() {
 				err = json.Unmarshal(b, &r)
 				r.Type = request.Type // This is not populated by Json unmarshal
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(r).Should(BeEquivalentTo(request))
+				Ω(r).ShouldNot(BeEquivalentTo(request))
 			})
 		})
 
